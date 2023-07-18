9b7e021fb288a3ac6770237867d65cb1f35e90ff
qisayouran <qisayouran@gmail.com>
2023-07-18 18:09:44 +0800

    修复bug

--- src/router/index.js
+++ src/router/index.js
@@ -10,7 +10,7 @@ const router = new VueRouter({
     {
       path: "/",
       name: "SubConverter",
-      component: () => import("@/views/SubConverter.vue"),
+      component: () => import("..Subconverter.vue"),
     },
   ],
 })
