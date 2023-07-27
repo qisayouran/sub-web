6e65d5b171f50015a0f58682d0dca8cdc1f06610
qisayouran <qisayouran@gmail.com>
2023-07-27 15:03:58 +0800

    更改时长

--- src/views/SubConverter.vue
+++ src/views/SubConverter.vue
@@ -622,7 +622,7 @@ export default {
         { style: "color: teal" },
         "各种订阅链接（短链接服务除外）生成纯前端实现，无隐私问题。默认提供后端转换服务，隐私担忧者请自行搭建后端服务"
       ),
-      duration: "1250",
+      duration: "2000",
       type: "warning",
     })
   },
